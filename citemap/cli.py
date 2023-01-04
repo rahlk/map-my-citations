2364e50dc080c239bccae5d6fa4b60c53176e0ba
Rahul Krishna <rkrsn@ibm.com>
2023-01-04 09:01:50 -0800

    Citemap made pip ready...

--- citemap/cli.py
+++ citemap/cli.py
@@ -7,12 +7,17 @@ import rich_click as click
 
 # CLI app configuration
 @click.command()
-@click.option("--author-id", help="Your google scholar author id. For example, "
-                                  "if your URL is https://scholar.google.com/citations?user=WGggocoAAAAJ&hl=en, "
-                                  "then your author_id is the  part after user= and before &hl=en, i.e., WGggocoAAAAJ")
-@click.option("--serp-api-token", envvar="SERP_API_TOKEN", default=None, help="Your serp api token")
-@click.option("--output-dir", help="The directory to save the outputs", default=Path(os.getcwd()), show_default=True)
-def cli(author_id, serp_api_token, output_dir):
+@click.option("--author-id", required=True, help="Your google scholar author id. For example, if your URL is "
+                                                 "https://scholar.google.com/citations?user=WGggocoAAAAJ&hl=en, "
+                                                 "then your author_id is the  part after user= and before &hl=en, "
+                                                 "i.e., WGggocoAAAAJ")
+@click.option("--serp-api-token", envvar="SERP_API_TOKEN", required=True, help="Your serp api token")
+@click.option("--output-dir",
+              help="The directory to save the outputs",
+              default=Path(os.getcwd()).joinpath('etc', 'citemap_output'),
+              type=click.Path(path_type=Path), show_default=True)
+def cli(author_id: str, serp_api_token: str, output_dir: Path):
     """A small python app to generate and plot your Google Scholar citations."""
+    output_dir.mkdir(exist_ok=True)
     citations_mapper(author_id, serp_api_token, output_dir)
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -59,3 +59,29 @@ setup(
         ],
     },
 )
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
